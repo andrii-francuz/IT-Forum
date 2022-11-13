2f756aa9c6d16852f8f2378927e952ab326746bd
andrii-francuz <andrii.fryntsko@gmail.com>
2022-11-13 22:37:01 +0100

    Added some styles in index.js for PostCard.js

--- components/PostCard.js
+++ components/PostCard.js
@@ -1,7 +1,9 @@
 import Image from "next/image"
 import Logo from "../static/logo.png"
+import {FiBookmark} from 'react-icons/fi'
 
 const styles = {
+    wrapper: `flex max-w-[46rem] h-[10rem] items-center gap-[1rem] cursor-pointer`,
     authorContainer: `flex gap-[0.4rem]`,
     authorImageContainer: `grid place-items-center rounded-full overflow-hidden h-[1.4rem] w-[1.4rem]`,
     authorImage: `object-cover`,
@@ -11,30 +13,45 @@ const styles = {
     detailsContainer: `flex items-center justify-between text-[#787878]`,
     articleDetails: `my-2 text-[0.8rem]`,
     category: `bg-[#F2F3F2] p-1 rounded-full`,
-    postDetails: ``,
+    bookmarkContainer: `cursor-pointer`,
+    thumbnailContainer: ``,
+    postDetails: `flex-[2.5] flex flex-col`,
 }
 
 const PostCard = () => {
     return(
-        <div className={styles.postDetails}>
-            <div className={styles.authorContainer}>
-                <div className={styles.authorImageContainer}>
-                    <Image
-                        src={Logo}
-                        className={styles.authorImage}
-                        width={40}
-                        height={40}
-                    />
+        <div className={styles.wrapper}>
+            <div className={styles.postDetails}>
+                <div className={styles.authorContainer}>
+                    <div className={styles.authorImageContainer}>
+                        <Image
+                            src={Logo}
+                            className={styles.authorImage}
+                            width={40}
+                            height={40}
+                        />
+                    </div>
+
+                    <div className={styles.authorName}>Andrii Fryntsko</div>
                 </div>
 
-                <div className={styles.authorName}>Andrii Fryntsko</div>
+                <h1 className={styles.title}>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h1>
+                <div className={styles.briefing}>Lorem ipsum dolor sit amet</div>
+
+                <div className={styles.detailsContainer}>
+                    <span className={styles.articleDetails}>Nov 13, 5 min read,
+                    <span className={styles.category}>productivity</span></span>
+                    <span className={styles.bookmarkContainer}>
+                        <FiBookmark className='h-5 w-5'/>
+                    </span>
+                </div>
             </div>
-
-            <h1 className={styles.title}>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h1>
-            <div className={styles.briefing}>Lorem ipsum dolor sit amet</div>
-
-            <div className={styles.detailsContainer}>
-                <span className={styles.articleDetails}>Nov 13, 5 min read, <span className={styles.category}>productivity</span></span>
+            <div className={styles.thumbnailContainer}>
+                <Image
+                    height={100}
+                    width={100}
+                    src={Logo}
+                />
             </div>
         </div>
     )
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,25 @@
 import Header from "../components/Header"
 import PostCard from "../components/PostCard"
 
+const styles = {
+  postList: `flex flex-col gap-3 p-2 sm:grid-cols-2 md:gap-6 md:p-6 lg:grid-cols-3`,
+  container: `max-w-7xl flex-1`,
+  main: `flex justify-center`,
+}
+
 export default function Home() {
-  const styles = {}
   return (
     <div>
       <Header/>
-      <PostCard/>
+      <div className={styles.main}>
+        <div className={styles.container}>
+          <div className={styles.postList}>
+            <PostCard/>
+            <PostCard/>
+            <PostCard/>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
